0e789910461235479060675cd5cef5351777261f
hanjjungwoo <eod0323@naver.com>
2021-11-22 16:06:12 +0900

    header
    업데이트

--- src/component/survey_result.js
+++ src/component/survey_result.js
@@ -1,7 +1,10 @@
 const SurveyResult = () => {
     return (
         <div className="form-background">
-            <p>결과페이지입니다</p>
+            <div className="result-background">
+                <h2 className="visit-h2">체크한 항목과 맞는 여행지는 "O O O" 입니다</h2>
+            </div>
+            
         </div>
         
     )
