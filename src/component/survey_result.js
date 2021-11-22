b92692490b5745925eb90808c2a60444065a05d0
hanjjungwoo <eod0323@naver.com>
2021-11-22 16:10:42 +0900

    !!
    !

--- src/component/survey_result.js
+++ src/component/survey_result.js
@@ -2,7 +2,8 @@ const SurveyResult = () => {
     return (
         <div className="form-background">
             <div className="result-background">
-                <h2 className="visit-h2">체크한 항목과 맞는 여행지는 "O O O" 입니다</h2>
+                <h2 className="visit-h2">체크한 항목과 맞는 여행지는 "O O O" 입니다! !</h2>
+                
             </div>
             
         </div>
