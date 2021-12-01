4bde85e3dc15f3f9b874a1c6448b2c7e43c3359a
hanjjungwoo <eod0323@naver.com>
2021-12-01 14:07:58 +0900

    result-page

--- src/component/visit_result.js
+++ src/component/visit_result.js
@@ -7,17 +7,26 @@ const VisitResult = () => {
                 <div className="result-img">
                     <a href="https://m.insight.co.kr/news/279928"><img className="result-img" src="https://i.pinimg.com/564x/64/0c/70/640c70e294f7f7594a962cb2b1ba74a4.jpg" alt="Klematis" width="100%" height="100%"/></a>
                 </div>
+                <div className="result-img-explain">
+                    관광지설명
+                </div>
                 <hr/>
                 <h2 className="result-h2">두번째 추천 관광지는 "" 입니다</h2>
                 <hr/>
                 <div className="result-img">
                     <a href="https://m.insight.co.kr/news/279928"><img className="result-img" src="https://i.pinimg.com/564x/64/0c/70/640c70e294f7f7594a962cb2b1ba74a4.jpg" alt="Klematis" width="100%" height="100%"/></a>
                 </div>
+                <div className="result-img-explain">
+                    관광지설명
+                </div>
                 <hr/>
                 <h2 className="result-h2">세번째 추천 관광지는 "" 입니다</h2>
                 <hr/>
                 <div className="result-img">
                     <a href="https://m.insight.co.kr/news/279928"><img className="result-img" src="https://i.pinimg.com/564x/64/0c/70/640c70e294f7f7594a962cb2b1ba74a4.jpg" alt="Klematis" width="100%" height="100%"/></a>
+                </div>
+                <div className="result-img-explain">
+                    관광지설명
                 </div>
             </div>
         </div>
