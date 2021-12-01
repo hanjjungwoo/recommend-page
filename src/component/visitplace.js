424a26de616643f5e09f19f0318d5b0629632803
hanjjungwoo <eod0323@naver.com>
2021-12-01 14:01:47 +0900

    result-page

--- src/component/visitplace.js
+++ src/component/visitplace.js
@@ -6,7 +6,7 @@ const Visit = () => {
     return (
         <div className="form-background">
             <div className="request-background">
-                <h3 className="result-h2">좋았던 여행지는 어디입니까?</h3>
+                <h1>좋았던 여행지는 어디입니까?</h1>
                 <Scroll/>
                 <Link to="/visit_result"><button type="submit">추천받기</button></Link>
             </div>
